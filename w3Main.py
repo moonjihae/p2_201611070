81d0b97a8386a2111d1661c2f9ac89a4ce32933c
moonjihae <dhfptwlqlc@gmail.com>
2016-03-24 21:36:44 +0900

    201611070 문지해
    w3Main

---
+++ w3Main.py
@@ -0,0 +1,9 @@
+﻿def T():
+        a=raw_input("F or C:")
+        temp=input("user input temperature:")
+        if a=='F':
+                result=(temp-32)/1.8
+                print(result,"C")
+        elif a=="C":
+                    result=temp*1.8+32
+                    print(result,"F")
