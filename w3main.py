1a13465828b2b67f7bc9af7592591dd86ba22dfe
moonjihae <dhfptwlqlc@naver.com>
2016-03-24 21:23:47 +0900

    Delete w3main.py

--- w3main.py
+++
@@ -1,9 +0,0 @@
-﻿def T():
-        a=raw_input("F or C:")
-        temp=input("user input temperature:")
-        if a=='F':
-                result=(temp-32)/1.8
-                print(result,"C")
-        elif a=="C":
-                    result=temp*1.8+32
-                    print(result,"F")
