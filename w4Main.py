f383d1972ff6dd3f7ccadca79a2c4c783197689b
moonjihae <dhfptwlqlc@gmail.com>
2016-03-29 20:57:29 +0900

    w4Main

---
+++ w4Main.py
@@ -0,0 +1,11 @@
+﻿import turtle
+wn=turtle.Screen()
+t1=turtle.Turtle()
+def wind(size,turns,sides,angle,bigger):
+    for i in range(sides):
+        if(i%2):
+            size=size+bigger
+            t1.fd(size)
+            t1.right(angle)
+wind(20,20,10,90,30)
+wn.exitonclick()
