79bf96055f2fe209356da8fd31981cc155895a53
moonjihae <dhfptwlqlc@gmail.com>
2016-03-30 12:55:09 +0900

    w5Main

---
+++ w5Main.py
@@ -0,0 +1,14 @@
+﻿def grade():
+    g=input("get your score:")
+    grade=int(g)
+    if 100>grade>=90:
+        print ("A")
+    if 90>grade>=80:
+        print("B")
+    if 80>grade>=70:
+        print("C")
+    if 70>grade>=60:
+        print("D")
+    if 60>grade:
+        print("F")
+grade()
