9cbd48a160822252652b9044cae0c939f5b4e18d
moonjihae <dhfptwlqlc@gmail.com>
2016-04-04 16:26:29 +0900

    0404 homework

---
+++ w5Main4.py
@@ -0,0 +1,14 @@
+﻿def bmi():
+    height=input("height: ")
+    weight=input("weight: ")
+    print "%.1f"%height, "%.1f" % weight
+    bmi=weight*10000/(height*height)
+    print "%.1f" %bmi
+    if bmi <=18.5:
+        print 'underweight'
+    elif bmi>=18.5 and bmi<=23:
+        print 'normalweight'
+    elif bmi>=23 and bmi<=25:
+        print 'overweight'
+    elif bmi>=25 and bmi<=30:
+        print 'obesity'
