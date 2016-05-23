fd98295d89ad3909d2d1d4587ed3052a86a1c55a
moonjihae <dhfptwlqlc@gmail.com>
2016-05-23 20:58:11 +0900

    0523

---
+++ w12Main/w12Main.py
@@ -0,0 +1,47 @@
+﻿import os
+mydir=os.getcwd()
+
+def dataone():
+    mydir=os.getcwd()
+    filename='python.txt'
+    myfilename=os.path.join(mydir,filename)
+    myfile=open(myfilename,'r')
+    try:
+        for line in myfile:
+            if line.find('Python')>=0:
+                print line
+        myfile.close()  
+    except IOError as e :
+        print e
+
+def datatwo():
+    file2=open('output.txt','w')
+    line1='first line\n'
+    file2.write(line1)
+    line2='\tsecond line \n'
+    file2.write(line2)
+    line3='third'
+    file2.write(line3)
+    file2.close()
+    myfile2=open('output.txt','r')
+    contentstwo=myfile2.readlines()
+    for a in range(0,len(contentstwo)):
+        if contentstwo[a].find('line') >= 0:
+                result = contentstwo[a]
+                print result.upper()
+        myfile2.close()
+
+def lab12():
+    print "Homework1"
+    dataone()
+    print "Homework2"
+    datatwo()
+
+def main():
+    lab12()
+    
+
+
+
+main()
+raw_input("")
