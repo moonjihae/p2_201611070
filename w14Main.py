14c342d5124af89ee90d403d0027f0e3e6d4ef7e
moonjihae <dhfptwlqlc@gmail.com>
2016-06-02 10:28:08 +0900

    0601 강아지 소리

---
+++ w14Main.py
@@ -0,0 +1,23 @@
+﻿class Dog(object):
+    def __init__(self,name):
+        self.name=name
+    def getName(self):
+        print "My dog name is ",self.name
+        
+    def talk(self,name):
+        print "mung mung"
+        
+class ShihTzuDog(object):
+    def talk(self,name):
+        print "si si"
+
+class Maltese(object):
+    def talk(self,name):
+        print "mal mal"
+mydog.getName()
+mydog.talk('dog')
+shi=ShihTzuDog()
+shi.talk('ShihTzuDog')
+mal=Maltese()
+mal.talk('Maltese')
+raw_input("ddd")
